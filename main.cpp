95a51e540f5f60574b5af4739980b021e5a69e64
Kacper Kiełbasa <kacper.kielbasa5000@gmail.com>
2025-10-05 12:08:07 +0200

    połączenie dwóch funkcji do wypisywania macierzy

--- main.cpp
+++ main.cpp
@@ -20,22 +20,49 @@ void print_m(std::vector<std::vector<int>> mat){
 	printf("]\n");
 }
 
+void print(const std::vector<int> &vec){
+	std::cout << "[ ";
+	for(auto x : vec){
+		std::cout << x << " ";
+	}
+	std::cout << "]\n";
+}
+
+void print(const std::vector<std::vector<int>> & mat){
+	std::cout << "[\n";
+
+	for(const auto  & row : mat){
+		printf("  ");
+		print(row);
+	}
+
+	std::cout << "]\n";
+}
+
 
 int main(){
-	std::string input = "";
+	// std::string input = ""; // na potem -> dodac dynamiczne dodawanie macierzy
 
 	std::vector<int> vec1 = {1, 2, 3};
 	std::vector<int> vec2 = {4, 5, 6};
 	std::vector<int> vec3 = {7, 8, 9};
 	std::vector<std::vector<int>> mat = {vec1, vec2, vec3};
 
-	std::vector<int> v1 = {1, 2};
-	std::vector<int> v2 = {3, 4};
-	std::vector<std::vector<int>> m = {v1, v2};
+	// std::vector<int> v1 = {1, 2};
+	// std::vector<int> v2 = {3, 4};
+	// std::vector<std::vector<int>> m = {v1, v2};
 
-	print_m(mat);
-	print_m(m);
-	print_m({{1, 2, 3}, {4, 5, 6}});
+	// print_m(mat);
+	// print_m(m);
+	// print_m({{1, 2, 3}, {4, 5, 6}});
+
+	// print(v1);
+	// print(v2);
+	// print(m);
+	// print(vec1);
+	// print(vec2);
+	print(mat);
+	print({{1,2}, {3,4}});
 
 	// std::cin >> input;
 	// printf(" > %s\n", input.c_str());
